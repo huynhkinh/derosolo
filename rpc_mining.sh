e6bee740d86baee20aeee19a8caa7d2d1dcd74bb
huynhkinh <164724768+huynhkinh@users.noreply.github.com>
2024-08-27 17:21:51 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qy3jcv4xa7hpd0fs8rcmkqr9efgpx9zgjcfru8jh8mkuz5xps0zl7qgad5cpy -r nodent2.cpumining.cloud:10100 -m 7 -p rpc;
+    sleep 5;
+done
