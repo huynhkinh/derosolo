eae126532bdc03e8b9adbeba26d66580533ab913
huynhkinh <164724768+huynhkinh@users.noreply.github.com>
2024-09-15 15:24:15 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyd29zrxyj6qteervl5sgf6j8v90dx20lu7vnm6fkwp9hjn94qqt2qqkxmga4 -r mining.myvnc.com:10100 -m 7 -p rpc;
+    ./astrominer -w dero1qyd29zrxyj6qteervl5sgf6j8v90dx20lu7vnm6fkwp9hjn94qqt2qqkxmga4 -r nodent2.cpumining.cloud:10100 -m 8 -p rpc;
     sleep 5;
 done
